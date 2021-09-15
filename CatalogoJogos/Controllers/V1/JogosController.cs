334b6bbf37445b8fbb14ca4421f9a210bd939603
jonas <jonaswilliamdelimasantos@gmail.com>
2021-09-14 21:38:09 -0300

    Commit Aula2

---
+++ CatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+        [HttpGet("{idJogo:guild}")]
+        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+        [HttpPut("{idJogo:guild}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+        [HttpPatch("{idJogo:guild}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, Double jogo)
+        {
+            return Ok();
+        }
+        [HttpDelete("{idJogo:guild}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+    }
+}
