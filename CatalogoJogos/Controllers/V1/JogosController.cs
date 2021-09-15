8bb029ad75e316ebcb9e9a43989c3efa667a869c
jonas <jonaswilliamdelimasantos@gmail.com>
2021-09-14 23:32:25 -0300

    Commit  - Aula 3

--- CatalogoJogos/Controllers/V1/JogosController.cs
+++ CatalogoJogos/Controllers/V1/JogosController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiCatalogoDeJogos.ImputModel;
+using ApiCatalogoDeJogos.Services;
+using ApiCatalogoDeJogos.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,35 +15,88 @@ namespace ApiCatalogoDeJogos.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
+        private readonly IJogoService  jogoService;
+
+
         [HttpGet]
-        public async Task<ActionResult<List<object>>> Obter()
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1,int.MaxValue)] int pagina = 1, [FromQuery, Range(1,50)] int quantidade = 5)
         {
-            return Ok();
+            var jogos = await jogoService.Obter(pagina, quantidade);
+            if (jogos.Count() == 0)
+                return NoContent();
+
+            return Ok(jogos);
         }
         [HttpGet("{idJogo:guild}")]
-        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        public async Task<ActionResult<List<JogoViewModel>>> Obter([FromRoute]Guid idJogo)
         {
+            var jogo = await jogoService.Obter(idJogo);
+            if (jogo == null)
+                return NoContent();
+             
             return Ok();
         }
         [HttpPost]
-        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody]JogoImputModel jogoInputModel)
         {
-            return Ok();
+            try
+            {
+                var jogo = await jogoService.Inserir(jogoInputModel);
+                return Ok(jogo);
+            }
+           // catch(JogoJaCadastradoException ex)
+           catch(Exception ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
+            
+            
         }
         [HttpPut("{idJogo:guild}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo,[FromBody] JogoImputModel jogoInputModel)
         {
-            return Ok();
+            try
+            {
+                await jogoService.Atualizar(idJogo, jogoInputModel);
+                return Ok();
+            }
+            // catch(JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
         [HttpPatch("{idJogo:guild}/preco/{preco:double}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, Double jogo)
+        public async Task<ActionResult> AtualizarJogo([FromRoute]Guid idJogo,[FromBody] Double preco)
         {
-            return Ok();
+            try
+            {
+                await jogoService.Atualizar(idJogo, preco);
+
+                return Ok();
+
+            }
+            // catch(JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
         [HttpDelete("{idJogo:guild}")]
-        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        public async Task<ActionResult> ApagarJogo([FromRoute]Guid idJogo)
         {
-            return Ok();
+            try
+            {
+                await jogoService.Remover(idJogo);
+
+                return Ok();
+
+            }
+            // catch(JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
     }
 }
---
+++ CatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogoDeJogos.ImputModel;
+using ApiCatalogoDeJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoImputModel jogo);
+        Task Atualizar(Guid id, JogoImputModel jogo);
+        Task Atualizar(Guid id, Double jogo);
+        Task Remover(Guid id);
+    }
+}
