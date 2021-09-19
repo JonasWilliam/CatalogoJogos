b514acf743fabd69c1ca7ba9052b8890ad5389d9
jonas <jonaswilliamdelimasantos@gmail.com>
2021-09-18 22:55:20 -0300

    Commit - Aula 4

---
+++ CatalogoJogos/Repositorios/IJogoRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCatalogoDeJogos.Entities;
+
+namespace ApiCatalogoDeJogos.Repositorios
+{
+   public interface IJogoRepositorio : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+
+
+    }
+}
